95de2d91a628349c8c7ea91b2bf41670fe84854e
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-06 14:11:14 -0600

    added the first TODO and const's to index.js

---
+++ Develop/index.js
@@ -0,0 +1,4 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');  // https://www.npmjs.com/package/inquirer
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
